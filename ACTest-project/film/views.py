5330cd9e3e49b2727b2aeab971096b8abfb6cf1a
Juunicacio <juhnicacio.bio@gmail.com>
2022-02-22 01:09:56 +0100

    Re-factored views.py file for tags and genres.

--- ACTest-project/film/views.py
+++ ACTest-project/film/views.py
@@ -6,45 +6,32 @@ from core.models import Tag, Genre
 
 from film import serializers
 
-
-class TagViewSet(viewsets.GenericViewSet, 
+class BaseFilmAttrViewSet(viewsets.GenericViewSet, 
 				mixins.ListModelMixin,
 				mixins.CreateModelMixin):
-	"""Manage tags in the database"""
+	"""Base viewset for user owned film attributes"""
+	"""Manage attributes in the database"""
 	authentication_classes = (TokenAuthentication, )
 	permission_classes = (IsAuthenticated, )
+
+	def get_queryset(self):
+		"""Return objects for the current authenticated user only"""
+		# Limiting objects to the authenticated user
+		return self.queryset.filter(user=self.request.user).order_by('-name')
+
+	# Perfom modifications to our create proccess
+	def perform_create(self, serializer):
+		"""Create a new object"""
+		serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseFilmAttrViewSet):
 	# Provide the queryset that you wanna return
 	queryset = Tag.objects.all()
 	serializer_class = serializers.TagSerializer
 
-	def get_queryset(self):
-		"""Return objects for the current authenticated user only"""
-		# Limiting objects to the authenticated user
-		return self.queryset.filter(user=self.request.user).order_by('-name')
 
-	# Perfom modifications to our create proccess
-	def perform_create(self, serializer):
-		"""Create a new tag"""
-		serializer.save(user=self.request.user)
-
-
-class GenreViewSet(viewsets.GenericViewSet, 
-				mixins.ListModelMixin,
-				mixins.CreateModelMixin):
-	"""Manage genres in the database"""
-	authentication_classes = (TokenAuthentication, )
-	permission_classes = (IsAuthenticated, )
-	# Provide the queryset that you wanna return
+class GenreViewSet(BaseFilmAttrViewSet):
 	queryset = Genre.objects.all()
 	serializer_class = serializers.GenreSerializer
-
-	def get_queryset(self):
-		"""Return objects for the current authenticated user only"""
-		# Limiting objects to the authenticated user
-		return self.queryset.filter(user=self.request.user).order_by('-name')
-
-	# Perfom modifications to our create proccess
-	def perform_create(self, serializer):
-		"""Create a new genre"""
-		serializer.save(user=self.request.user)
 
